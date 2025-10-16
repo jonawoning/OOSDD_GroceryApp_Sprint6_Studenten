a13727eae67def7e99bea1a706efcfd76d526fc4
jonawoning <jonawoningyt@gmail.com>
2025-10-16 15:26:25 +0200

    Toevoegen van database logica aan product repository
    - Toevoegen van database logica aan product repository

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Models;
+using Microsoft.Data.Sqlite;
 
 namespace Grocery.Core.Data.Repositories
 {
@@ -9,38 +10,127 @@ namespace Grocery.Core.Data.Repositories
         
         public ProductRepository()
         {
-            _products = [
-                new Product(1, "Melk", 300, new DateOnly(2025, 9, 25), 0.95m),
-                new Product(2, "Kaas", 100, new DateOnly(2025, 9, 30), 7.98m),
-                new Product(3, "Brood", 400, new DateOnly(2025, 9, 12), 2.19m),
-                new Product(4, "Cornflakes", 0, new DateOnly(2025, 12, 31), 1.48m)];
+            CreateTable(@"CREATE TABLE IF NOT EXISTS Product (
+                            [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                            [Name] NVARCHAR(80) UNIQUE NOT NULL,
+                            [Stock] INTEGER NOT NULL,
+                            [ExpirationDate] DATE NOT NULL,
+                            [Price] DECIMAL(10,2) NOT NULL);");
+            List<string> insertQueries = [
+                @"INSERT OR IGNORE INTO Product(Name, Stock, ExpirationDate, Price) VALUES('Melk', 300, '2025-09-25', 0.95)",
+                @"INSERT OR IGNORE INTO Product(Name, Stock, ExpirationDate, Price) VALUES('Kaas', 100, '2025-09-30', 7.98)",
+                @"INSERT OR IGNORE INTO Product(Name, Stock, ExpirationDate, Price) VALUES('Brood', 400, '2025-09-12', 2.19)",
+                @"INSERT OR IGNORE INTO Product(Name, Stock, ExpirationDate, Price) VALUES('Cornflakes', 0, '2025-12-31', 1.48)"
+            ];
+            InsertMultipleWithTransaction(insertQueries);
         }
         public List<Product> GetAll()
         {
+            _products.Clear();
+            string selectQuery = "SELECT Id, Name, Stock, date(ExpirationDate), Price FROM Product";
+            OpenConnection();
+            using (SqliteCommand command = new(selectQuery, Connection))
+            {
+                SqliteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+                    int stock = reader.GetInt32(2);
+                    DateOnly expirationDate = DateOnly.FromDateTime(reader.GetDateTime(3));
+                    decimal price = reader.GetDecimal(4);
+                    _products.Add(new(id, name, stock, expirationDate, price));
+                }
+            }
+            CloseConnection();
             return _products;
         }
 
         public Product? Get(int id)
         {
-            return _products.FirstOrDefault(p => p.Id == id);
+            // get product from database
+            string selectQuery = "SELECT Id, Name, Stock, date(ExpirationDate), Price FROM Product WHERE Id = @Id";
+            OpenConnection();
+            using (SqliteCommand command = new(selectQuery, Connection))
+            {
+                command.Parameters.AddWithValue("Id", id);
+                SqliteDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    int productId = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+                    int stock = reader.GetInt32(2);
+                    DateOnly expirationDate = DateOnly.FromDateTime(reader.GetDateTime(3));
+                    decimal price = reader.GetDecimal(4);
+                    CloseConnection();
+                    return new(productId, name, stock, expirationDate, price);
+                }
+            }
+            CloseConnection();
+            // Er is iets mis gegaan, dus een return null
+            return null;
         }
 
         public Product Add(Product item)
         {
-            throw new NotImplementedException();
+            // add product to database
+            string insertQuery = $"INSERT INTO Product(Name, Stock, ExpirationDate, Price) VALUES(@Name, @Stock, @ExpirationDate, @Price) Returning RowId;";
+            OpenConnection();
+            using (SqliteCommand command = new(insertQuery, Connection))
+            {
+                command.Parameters.AddWithValue("Name", item.Name);
+                command.Parameters.AddWithValue("Stock", item.Stock);
+                command.Parameters.AddWithValue("ExpirationDate", item.ShelfLife.ToString("yyyy-MM-dd"));
+                command.Parameters.AddWithValue("Price", item.Price);
+                item.Id = Convert.ToInt32(command.ExecuteScalar());
+            }
+            CloseConnection();
+            return item;
         }
 
         public Product? Delete(Product item)
         {
-            throw new NotImplementedException();
+            // delete product from database
+            string deleteQuery = $"DELETE FROM Product WHERE Id = @Id;";
+            OpenConnection();
+            using (SqliteCommand command = new(deleteQuery, Connection))
+            {
+                command.Parameters.AddWithValue("Id", item.Id);
+                int rowsAffected = command.ExecuteNonQuery();
+                CloseConnection();
+                if (rowsAffected == 0)
+                {
+                    return null; // Item niet gevonden, dus return null
+                }
+                return item; // Return the deleted item
+            }
         }
 
         public Product? Update(Product item)
         {
-            Product? product = _products.FirstOrDefault(p => p.Id == item.Id);
-            if (product == null) return null;
-            product.Id = item.Id;
-            return product;
+            // update product in database
+            string updateQuery = $"UPDATE Product SET Name = @Name, Stock = @Stock. ExpirationDate = @ExpirationDate, Price = @Price WHERE Id = @Id;";
+            OpenConnection();
+            using (SqliteCommand command = new(updateQuery, Connection))
+            {
+                command.Parameters.AddWithValue("Name", item.Name);
+                command.Parameters.AddWithValue("Stock", item.Stock);
+                command.Parameters.AddWithValue("ExpirationDate", item.ShelfLife.ToString("yyyy-MM-dd"));
+                command.Parameters.AddWithValue("Price", item.Price);
+                command.Parameters.AddWithValue("Id", item.Id);
+                int rowsAffected = command.ExecuteNonQuery();
+                CloseConnection();
+                if (rowsAffected == 0)
+                {
+                    return null; // Item niet gevonden, dus return null
+                }
+                return item; // Return the updated item
+            }
+            // Product? product = _products.FirstOrDefault(p => p.Id == item.Id);
+            // if (product == null) return null;
+            // product.Id = item.Id;
+            // return product;
         }
     }
 }
