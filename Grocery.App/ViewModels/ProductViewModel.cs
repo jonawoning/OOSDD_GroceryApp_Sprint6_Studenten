9bfcc413d586344740e3ff6eab2664a4559baa3c
jonawoning <jonawoningyt@gmail.com>
2025-10-16 15:41:32 +0200

    Nieuwe Product view toegevoegd
    - Nieuwe view toegevoegd voor NewProductView
    - Alvast een formulier gemaakt voor de nieuw product view

--- Grocery.App/ViewModels/ProductViewModel.cs
+++ Grocery.App/ViewModels/ProductViewModel.cs
@@ -1,19 +1,32 @@
 ﻿using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
 using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+using Grocery.App.Views;
 
 namespace Grocery.App.ViewModels
 {
-    public class ProductViewModel : BaseViewModel
+    public partial class ProductViewModel : BaseViewModel
     {
         private readonly IProductService _productService;
         public ObservableCollection<Product> Products { get; set; }
+        public Client Client { get; set; }
 
-        public ProductViewModel(IProductService productService)
+        public ProductViewModel(IProductService productService, GlobalViewModel globalViewModel)
         {
             _productService = productService;
             Products = [];
+            Client = globalViewModel.Client;
             foreach (Product p in _productService.GetAll()) Products.Add(p);
+        }
+
+        [RelayCommand]
+        public async Task ShowNewProduct()
+        {
+            if (Client.Role == Role.Admin)
+            {
+                await Shell.Current.GoToAsync(nameof(NewProductView), true);
+            }
         }
     }
 }
